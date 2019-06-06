681d4c1e7c7735142be87fa33baf875de653b00e
BreyBatten <47111428+BreyBatten@users.noreply.github.com>
2019-06-05 23:07:44 -0400

    changed line of code to grab the correct movie id

--- client/src/App.js
+++ client/src/App.js
@@ -25,7 +25,7 @@ export default class App extends Component {
         <SavedList list={this.state.savedList} />
         <div>
           <Route exact path="/" component={MovieList} />
-          <Route path="/movie/:id" component={Movie} />
+          <Route path="/movie-list/:id" component={Movie} />
         </div>
       </div>
     );
